43b273ee4856d86798ae8992f576b606d5415547
Yazukee <yasser.almaliki@live.se>
2025-03-19 16:46:06 +0100

    Justeringar förbättringar

--- ehandel/app/(root)/page.tsx
+++ ehandel/app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import ProductList from "@/components/ui/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return <>Ehandel</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title="Latest trend" limit={4} />
+    </>
+  );
 };
 
 export default HomePage;
